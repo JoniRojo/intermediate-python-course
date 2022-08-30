7413da470d9b69052cd47dd95ee71a60623c1639
Jonathan Rojo <86434164+JoniRojo@users.noreply.github.com>
2022-08-30 01:21:43 -0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -11,7 +11,7 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     print(f'You rolled a {roll}')
-    dice_sum = dice_sum + rol
+    dice_sum = dice_sum + roll
     
   print(f'You have rolled a total of {dice_sum}')
 
