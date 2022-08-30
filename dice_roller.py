4a754b14890a059ffe759cdaf09ad53de9718aa7
Jonathan Rojo <86434164+JoniRojo@users.noreply.github.com>
2022-08-30 01:17:17 -0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,14 @@
 import random
 def main():
   print('You rolled a die')
- 
+  
+  dice_rolls = 2
   l = random.randint(1,6)
   print(f"You rolled a {l}")
+  
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
